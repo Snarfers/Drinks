c18ebcba96d9a9e5797adb85c4ecc5e7e7eb92e8
Tyler Walker <twalkerweb@gmail.com>
2016-07-08 22:39:21 -0400

    adding web files

--- web/js/app.js
+++ web/js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('drinks', ['ngRounter']);
 app.config(function($stateProvider, $urlRouterProvider){
     $stateProvider.state('ingredientListing', {
         url: '/ingredient/all',
-        templateUrl: '/'
+        templateUrl: '/',
         controller: 'IngredientListingController as ingListCtrl'
     });
 });
