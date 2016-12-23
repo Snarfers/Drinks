60e5fc1e144e148d2eec79fdbfae942f69ce3f53
Tyler Walker <twalkerweb@gmail.com>
2016-12-23 13:19:00 -0800

    Changing to allow for commands

--- api/server.py
+++ api/server.py
@@ -1,18 +1,21 @@
-import sys 
+import sys
 
 from app import app
 
 from app.routes.ingredient import ingredient_index, ingredient_category, ingredient_all, ingredient_create
 from app.routes.recipe import recipe_index, recipe_all, recipe_create
 
-if (sys.argv[1] == "run"):
-  app.run(debug=True, use_reloader=False)
+def main():
+    if (len(sys.argv) == 1):
+        app.run(debug=True, use_reloader=False)
+    elif (sys.argv[1] == "run"):
+        app.run(debug=True, use_reloader=False)
+    elif (sys.argv[1] == "create"):
+        # TODO: Create the database
+        pass
+    elif (sys.argv[1] == "seed"):
+        # TODO: Seed the database
+        pass
 
-if (sys.argv[1] == "create"):
-  # TODO: Create the database
-  pass
-
-if (sys.argv[1] == "seed"):
-  # TODO: Seed the database
-  pass 
- 
+if __name__ == "__main__":
+    main()
