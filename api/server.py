a8d7b7a540fe5605df78ef788132b64359b5b9e8
Tyler Walker <twalkerweb@gmail.com>
2016-12-23 12:37:04 -0800

    Updating how the server calls functions.

--- api/server.py
+++ api/server.py
@@ -1,7 +1,18 @@
+import sys 
+
 from app import app
 
 from app.routes.ingredient import ingredient_index, ingredient_category, ingredient_all, ingredient_create
 from app.routes.recipe import recipe_index, recipe_all, recipe_create
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+if (sys.argv[1] == "run"):
+  app.run(debug=True, use_reloader=False)
+
+if (sys.argv[1] == "create"):
+  # TODO: Create the database
+  pass
+
+if (sys.argv[1] == "seed"):
+  # TODO: Seed the database
+  pass 
+ 
