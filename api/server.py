27a30ac122eaa2673ebaaf0d03ce85aec40b4710
Tyler Walker <twalkerweb@gmail.com>
2016-12-23 16:56:28 -0800

    Removing the runtime logic from the main function

--- api/server.py
+++ api/server.py
@@ -5,17 +5,26 @@ from app import app
 from app.routes.ingredient import ingredient_index, ingredient_category, ingredient_all, ingredient_create
 from app.routes.recipe import recipe_index, recipe_all, recipe_create
 
+def run_server():
+    app.run(debug=True, use_reloader=False)
+
+def seed_database():
+    # TODO: Create the database
+    pass
+
+def create_database():
+    # TODO: Seed the database
+    pass
+
 def main():
     if (len(sys.argv) == 1):
-        app.run(debug=True, use_reloader=False)
+        run_server()
     elif (sys.argv[1] == "run"):
-        app.run(debug=True, use_reloader=False)
+        run_server()
     elif (sys.argv[1] == "create"):
-        # TODO: Create the database
-        pass
+        create_database()
     elif (sys.argv[1] == "seed"):
-        # TODO: Seed the database
-        pass
+        seed_database()
 
 if __name__ == "__main__":
     main()
