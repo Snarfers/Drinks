d630a5bc939021a4f92393dde7e5b384dd52ac24
Tyler Walker <twalkerweb@gmail.com>
2016-12-23 17:05:35 -0800

    Making sure ththe default exits before hitting #BadCode

--- api/server.py
+++ api/server.py
@@ -7,24 +7,36 @@ from app.routes.recipe import recipe_index, recipe_all, recipe_create
 
 def run_server():
     app.run(debug=True, use_reloader=False)
+    exit()
 
-def seed_database():
-    # TODO: Create the database
-    pass
 
 def create_database():
     # TODO: Seed the database
     pass
 
+
+def seed_database():
+    # TODO: Create the database
+    pass
+
+
 def main():
+    # Default action: run the server
     if (len(sys.argv) == 1):
         run_server()
-    elif (sys.argv[1] == "run"):
+
+    # Run the server
+    if (sys.argv[1] == "run"):
         run_server()
-    elif (sys.argv[1] == "create"):
+
+    # Create the database if it doesn't exist
+    if (sys.argv[1] == "create"):
         create_database()
-    elif (sys.argv[1] == "seed"):
+
+    # Seed the database
+    if (sys.argv[1] == "seed"):
         seed_database()
+
 
 if __name__ == "__main__":
     main()
